f4ec1dfa962a4d5549b1d0a5f3f32f7d6dbd17ed
Wesley Tokoi <tokoiwesley@gmail.com>
2026-01-20 17:33:39 +0300

    feat: randomise the dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -33,7 +34,7 @@ class _DicePageState extends State<DicePage> {
               icon: Image.asset('images/dice$leftDiceNumber.png'),
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = 5;
+                  leftDiceNumber = Random().nextInt(6) + 1;
                 });
               },
             ),
@@ -42,7 +43,9 @@ class _DicePageState extends State<DicePage> {
             child: IconButton(
               icon: Image.asset('images/dice$rightDiceNumber.png'),
               onPressed: () {
-                print('Right button got pressed');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
               },
             ),
           ),
